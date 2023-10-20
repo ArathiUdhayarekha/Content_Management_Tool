d0ca8371acbe7e1cad3ac7617fb98ed4401f50f5
ArathiUdhayarekha <148026181+ArathiUdhayarekha@users.noreply.github.com>
2023-10-20 13:48:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 function saveContent() {
     var title = document.getElementById('contentTitle').value;
     var body = document.getElementById('contentBody').value;
-
+ if(title && body)
+ {
     var contentList = document.getElementById('contentList');
     var listItem = document.createElement('li');
     listItem.innerText = `Title: ${title}\nBody: ${body}`;
     contentList.appendChild(listItem);
-
+     clearEditor();
+ }
+ else{
+        alter("please enter both title and content.");
+     }
+}
+function clearEditor()
+{
     document.getElementById('contentTitle').value = '';
     document.getElementById('contentBody').value = '';
 }
